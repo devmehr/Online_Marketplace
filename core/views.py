325ac701cfcaf4add9ad65bbd972bc09def4975d
Muhammad Ijlal <mijlalawan@gmail.com>
2024-06-16 20:50:35 +0500

    Products database & homepage crerated

--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from item.models import Catagory, Item
+
 def index(request):
-    return render(request, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    catagories = Catagory.objects.all()
+
+    return render(request, 'core/index.html', {
+                  'catagories': catagories,
+                  'items': items,
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
--- online_marketplace/urls.py
+++ online_marketplace/urls.py
@@ -1,3 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +12,4 @@ urlpatterns = [
     path('', index, name='index'),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
