cea753e3f0f6e402586cf922d6a760346c4bf3d7
Muhammad Ijlal <mijlalawan@gmail.com>
2024-06-16 22:02:39 +0500

    details of products crerated

--- item/views.py
+++ item/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Item
+
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+
+    related_items = Item.objects.filter(catagory=item.catagory, is_sold=False).exclude(pk=pk)[0:3]
+
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
