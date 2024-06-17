14ed8760ddf773cfdcf1d0e5b6f6682663dde94c
Muhammad Ijlal <mijlalawan@gmail.com>
2024-06-17 15:43:27 +0500

    Dashboard, adding and deletion of products created

--- item/views.py
+++ item/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
+from django.shortcuts import render, get_object_or_404, redirect
+
+from .forms import NewItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -12,3 +15,31 @@ def detail(request, pk):
         'item': item,
         'related_items': related_items
     })
+
+
+@login_required
+def new(request):
+
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = NewItemForm()
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'New Item',
+    })
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
