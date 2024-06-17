a28a5503aca92e667acc732d47049b945768af59
Muhammad Ijlal <mijlalawan@gmail.com>
2024-06-17 15:57:08 +0500

    Product details edit created

--- item/forms.py
+++ item/forms.py
@@ -24,3 +24,24 @@ class NewItemForm(forms.ModelForm):
                 'class': 'w-full py-4 px-6 rounded-xl border'
             })
         }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold',)
+
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'price': forms.TextInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            }),
+            'image': forms.FileInput(attrs={
+                'class': 'w-full py-4 px-6 rounded-xl border'
+            })
+        }
--- item/views.py
+++ item/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 def detail(request, pk):
@@ -36,6 +36,26 @@ def new(request):
         'form': form,
         'title': 'New Item',
     })
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+    })
+
+
+
 
 @login_required
 def delete(request, pk):
